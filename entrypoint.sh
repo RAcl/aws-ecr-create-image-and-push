8b4580e7569c015dc69131079061b53a194ed81f
Roy Alvear A <racl@gulix.cl>
2024-04-20 00:13:47 -0400

    clean script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-# Stop execute on error
-# set -e
-
 # show last AWS_ACCESS_KEY_ID
 KEY_ID=$(echo ${AWS_ACCESS_KEY_ID} | sed 's/.\{16\}/****************/g')
 echo "Use KEY_ID: ${KEY_ID}"
 
-# create workdirs
-# mkdir -p ~/{.aws,.kube}
-
-# create AWS's config files
-# cat > ~/.aws/credentials << EOF_CRED
-# [default]
-# aws_accesss_key_id = ${AWS_ACCESS_KEY_ID}
-# aws_secret_accesss_key = ${AWS_SECRET_ACCESS_KEY}
-# EOF_CRED
-
+# getAccountID
 AccountID=$(aws sts get-caller-identity | grep Account | awk -F'"' '{print $4}')
-
 echo "Account ID: ", $AccountID
 
+# login AWS ECR
 DKR_SRV="${AccountID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-
 aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin ${DKR_SRV}
 if [ $? != 0 ]; then
     echo "Error Login to AWS ECR"
@@ -31,12 +18,13 @@ else
     echo "AWS ECR login OK"
 fi
 
+# initialize
 TAG_SHA=$(echo $GITHUB_SHA | cut -c 1-7)
-
 [ -n "${AWS_REPOSITORY}" ]&&ECR_REPO="${AWS_REPOSITORY}"||ECR_REPO="${GITHUB_REPOSITORY}"
 [ -n "${LATEST}" ]||LATEST="latest"
 [ -n "${IMAGE_TAG}" ]&&REPOTAG="${IMAGE_TAG}"||REPOTAG="${TAG_SHA}"
 
+# build
 sh -c "docker build -t ${ECR_REPO}:${TAG_SHA} $* ."
 if [ $? != 0 ]; then
     echo "Error bad construction \"docker build -t ${ECR_REPO}:${TAG_SHA} $* .\""
@@ -49,7 +37,7 @@ docker tag $ECR_REPO:${TAG_SHA} $DKR_SRV/$ECR_REPO:$REPOTAG
 docker push $DKR_SRV/$ECR_REPO:$LATEST
 docker push $DKR_SRV/$ECR_REPO:$REPOTAG
 echo "IMAGE=$DKR_SRV/$ECR_REPO:$REPOTAG" >> $GITHUB_OUTPUT
-echo $DKR_SRV/$ECR_REPO:$REPOTAG
 
-# delete workdirs
-rm -rf ~/.aws
+# informar
+echo "publish ${DKR_SRV}/${ECR_REPO}:${$LATEST}"
+echo "export IMAGE=${DKR_SRV}/${ECR_REPO}:${REPOTAG}"
